1d80e7099f98ec899e91bd6f21b99397be07e937
Lebedev Anton <unknown.dev.511@gmail.com>
2023-11-29 18:41:52 +0300

    benchmarks

--- Test.Benchmark/Program.cs
+++ Test.Benchmark/Program.cs
@@ -5,7 +5,22 @@ BenchmarkRunner.Run<Benchmarks>();
 
 public class Benchmarks {
     [Benchmark]
-    public void Triangle() {
+    public void Triangle1() {
+        global::Triangle.Triangle.GetTriangleType(1.414213562373095d, 1, 1);
+    }
+
+    [Benchmark]
+    public void Triangle2() {
+        global::Triangle.Triangle.GetTriangleType(1, 1.414213562373095d, 1);
+    }
+
+    [Benchmark]
+    public void Triangle3() {
         global::Triangle.Triangle.GetTriangleType(1, 1, 1.414213562373095d);
+    }
+
+    [Benchmark]
+    public void Triangle4() {
+        global::Triangle.Triangle.GetTriangleType(1, 1, 1);
     }
 }
